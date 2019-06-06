7af3291992bbe5a8a4b85bdc0fa512b5234dbe31
kain <kainpro@foxmail.com>
2019-06-06 15:37:13 +0800

    Submit Prettierrc

--- 001-TwoSum.js
+++ 001-TwoSum.js
@@ -3,14 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    const map = new Map();
-    const len = nums.length;
-    for (var i = 0; i < len; i++) {
-        const key = target - nums[i];
-        if (map.has(key)) return [map.get(key), i];
-        map.set(nums[i], i);
-    }
+var twoSum = function(nums, target) {
+	const map = new Map();
+	const len = nums.length;
+	for (var i = 0; i < len; i++) {
+		const key = target - nums[i];
+		if (map.has(key)) return [map.get(key), i];
+		map.set(nums[i], i);
+	}
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
--- 002-AddTwoNumbers.js
+++ 002-AddTwoNumbers.js
@@ -1,6 +1,6 @@
 function ListNode(val) {
-    this.val = val;
-    this.next = null;
+	this.val = val;
+	this.next = null;
 }
 
 /**
@@ -8,42 +8,56 @@ function ListNode(val) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-    const node = new ListNode();
-    let remainder = 0, wait = true, cursor = node;
-    while (wait) {
-        const n1 = l1 && l1.val ? l1.val : 0;
-        const n2 = l2 && l2.val ? l2.val : 0;
-        let numbers = n1 + n2 + remainder;
-        if (numbers > 9) {
-            numbers -= 10;
-            remainder = 1;
-        } else {
-            remainder = 0;
-        }
-        cursor.val = numbers;
-        if ((l1 && l1.next) || (l2 && l2.next) || remainder) {
-            l1 = l1 && l1.next ? l1.next : null;
-            l2 = l2 && l2.next ? l2.next : null;
-            cursor = cursor.next = new ListNode();
-        } else {
-            wait = false;
-        }
-    }
-    return node;
+var addTwoNumbers = function(l1, l2) {
+	const node = new ListNode();
+	let remainder = 0, cursor = node;
+	while (l1 || l2) {
+		const n1 = l1 && l1.val ? l1.val : 0;
+		const n2 = l2 && l2.val ? l2.val : 0;
+		let numbers = n1 + n2 + remainder;
+		if (numbers > 9) {
+			numbers -= 10;
+			remainder = 1;
+		} else {
+			remainder = 0;
+		}
+		cursor.val = numbers;
+		l1 = l1 && l1.next ? l1.next : null;
+		l2 = l2 && l2.next ? l2.next : null;
+		if (!l1 && !l2) {
+			cursor.next = remainder ? new ListNode(remainder) : null;
+		} else {
+			cursor = cursor.next = new ListNode();
+		}
+	}
+	return node;
 };
 
-
 function setListNode(lists) {
-    const node = new ListNode(lists.shift());
-    let cursor = node;
-    while (lists.length !== 0) {
-        cursor.next = new ListNode(lists.shift());
-        cursor = cursor.next;
-    }
-    return node;
+	const node = new ListNode(lists.shift());
+	let cursor = node;
+	while (lists.length !== 0) {
+		cursor.next = new ListNode(lists.shift());
+		cursor = cursor.next;
+	}
+	return node;
 }
 
-console.log(addTwoNumbers(setListNode([2, 4, 3]), setListNode([5, 6, 4])));
-console.log(addTwoNumbers(setListNode([1, 8]), setListNode([0])));
-console.log(addTwoNumbers(setListNode([5]), setListNode([5])));
+console.log(
+	addTwoNumbers(
+		setListNode([2, 4, 3]),
+		setListNode([5, 6, 4]),
+	),
+);
+console.log(
+	addTwoNumbers(
+		setListNode([1, 8]),
+		setListNode([0]),
+	),
+);
+console.log(
+	addTwoNumbers(
+		setListNode([5]),
+		setListNode([5]),
+	),
+);
