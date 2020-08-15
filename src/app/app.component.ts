d54168174bddfc33df82596f08cea93049a8f0c5
kain <kainpro@foxmail.com>
2020-08-15 12:25:41 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   constructor(
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NgxMqttLiteService} from "ngx-mqtt-lite";
+import { AppComponent } from './app.component';
+import { NgxMqttLiteService } from 'ngx-mqtt-lite';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [
     NgxMqttLiteService
